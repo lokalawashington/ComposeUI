efc9116a05fbf0215d56e3eba355de90bc10464c
Washington <lokalawashington2019@gmail.com>
2023-07-10 13:30:56 +0300

    styling text

--- app/src/main/java/com/example/basiccompose/MainActivity.kt
+++ app/src/main/java/com/example/basiccompose/MainActivity.kt
@@ -3,90 +3,77 @@ package com.example.basiccompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Card
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.painter.Painter
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.example.basiccompose.ui.theme.BasicComposeTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            val painter = painterResource(id = R.drawable.compose)
-            val description = "Washington is learning Compose"
-            val title = "Jetpack Compose"
-            Box(modifier = Modifier
-                .fillMaxWidth(0.5f)
-                .padding(16.dp)) {
-                ImageCard(painter = painter, contentDescription = description, title = title)
+            val fontFamily = FontFamily(
+                Font(R.font.lexendthin, FontWeight.Thin),
+                Font(R.font.lexendlight, FontWeight.Light),
+                Font(R.font.lexendregular, FontWeight.Normal),
+                Font(R.font.lexendmedium, FontWeight.Medium),
+                Font(R.font.lexendsemibold, FontWeight.SemiBold),
+                Font(R.font.lexendbold, FontWeight.Bold),
+                Font(R.font.lexendextrabold, FontWeight.ExtraBold)
+
+
+            )
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(Color(0xFF101010))
+                    .padding(32.dp)
+            ) {
+                Text(
+                    text = buildAnnotatedString {
+                        withStyle(
+                            style = SpanStyle(
+                                color = Color.Green,
+                                fontSize = 50.sp
+                            )
+                        ) {
+                            append("j")
+                        }
+                        append("etpack")
+
+                        withStyle(
+                            style = SpanStyle(
+                                color = Color.Green,
+                                fontSize = 50.sp
+                            )
+                        ) {
+                            append("C")
+                        }
+                        append("ompose")
+                    } ,
+                    color = Color . White,
+                    fontSize = 30.sp,
+                    fontFamily = fontFamily,
+                    fontWeight = FontWeight.Bold,
+                    fontStyle = FontStyle.Italic,
+                    textAlign = TextAlign.Center,
+                    textDecoration = TextDecoration.Underline
+                )
             }
-            }
-
-    }
-}
-
-@Composable
-fun ImageCard(
-    painter: Painter,
-    contentDescription: String,
-    title: String,
-    modifier: Modifier = Modifier
-
-){
-    Card(
-       modifier = modifier.fillMaxWidth(),
-        shape = RoundedCornerShape(16.dp)
-    ){
-        Box(
-            modifier = Modifier.height(200.dp
-        )){
-            Image(painter = painter,
-                 modifier = Modifier.fillMaxSize(),
-                  contentDescription = contentDescription,
-                  contentScale = ContentScale.Crop
-                 )
-            Box(modifier = Modifier
-                .fillMaxSize()
-                .padding(12.dp)
-                .background(
-                    Brush.verticalGradient(
-                      colors = listOf(
-                          Color.Transparent, Color.Black
-                      )  ,
-                        startY = 300f
-                    )
-                ))
-            Box(modifier = Modifier
-                .fillMaxSize()
-                .padding(12.dp),
-                contentAlignment = Alignment.BottomStart
-            ){
-                Text(title, style = TextStyle(color = Color.White, fontSize = 16.sp))
-            }
-
         }
-
     }
-
 }
